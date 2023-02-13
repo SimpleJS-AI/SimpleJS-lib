3fbc9c16feab4616a4b60a0ded7e26305f383160
flug8 <80972789+flug8@users.noreply.github.com>
2023-02-13 16:04:11 +0100

    Initial commit

---
+++ simple.js
@@ -0,0 +1,10 @@
+
+// Neural Network
+
+class NeuralNetwork {
+    constructor(input_nodes, hidden_nodes, output_nodes) {
+        this.input_nodes = input_nodes;
+        this.hidden_nodes = hidden_nodes;
+        this.output_nodes = output_nodes;
+    }
+}
